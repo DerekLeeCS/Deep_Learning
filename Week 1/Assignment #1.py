c43c711a97588df230014a27fbe5d5c69752502a
Derek Lee <DerekLeeCS@users.noreply.github.com>
2020-09-06 20:03:55 -0400

    implemented module and broadcasting

--- Week 1/Assignment #1.py
+++ Week 1/Assignment #1.py
@@ -19,83 +19,98 @@ y = tf.sin( 2 * np.pi * x ) + eps
 
 # Used for graphing true sinewave without noise
 trueX = np.linspace( 0, 1, 500, dtype = 'float32' )
+trueX = np.reshape( trueX, (500,1) )    # Reshapes into vector to allow broadcasting
 trueY = np.sin( 2 * np.pi * trueX )
 
-# Trainable Tensorflow variables
-w = tf.Variable( tf.random.uniform( [M], -0.5, 0.5 ) )
-mu =  tf.Variable( tf.linspace( -0.1, 1.1, [M] ) )
-sig = tf.Variable( 0.25 * tf.ones( shape = [M] ) )
-b = tf.Variable( tf.random.uniform( [1], -0.5, 0.5 ) )
 
+# Module containing Linear Regression model
+class linRegMod( tf.Module ):
 
-# Loss function
-@tf.function
-def lossFunc( x, y ): 
-    
-    yHat = tf.map_fn( estY, x ) # Applies estY elementwise to x
-    MSE = tf.reduce_sum( 0.5 * ( y - yHat )**2 )
+    def __init__(self):
+
+        # Trainable Tensorflow variables
+        self.w = tf.Variable( tf.random.uniform( [M], -0.5, 0.5 ), name = 'w' )
+        self.mu =  tf.Variable( tf.linspace( -0.1, 1.1, [M] ), name = 'mu' )
+        self.sig = tf.Variable( 0.25 * tf.ones( shape = [M] ), name = 'sig' )
+        self.b = tf.Variable( tf.random.uniform( [1], -0.5, 0.5 ), name = 'b' )
+
+    # Loss function
+    @tf.function
+    def lossFunc( self, x, y ): 
         
-    return MSE
+        yHat = self.estY( x )   # Applies estY elementwise to x
+        MSE = tf.reduce_sum( 0.5 * ( y - yHat )**2 )
+            
+        return MSE
 
+    # Calculates yHat given x
+    @tf.function
+    def estY( self, x ):
 
-# Calculates yHat_i given x_i
-@tf.function
-def estY( x ):
+        return tf.reduce_sum( self.w * self.gaussian( x ), 1, keepdims = True ) + self.b   # Sums along rows to get N x 1
 
-    return tf.reduce_sum( w * gaussian( x, mu, sig ) ) + b
+    # Generates N x M matrix
+    def gaussian( self, x ):
 
+        return tf.math.exp( -( x - self.mu )**2 / self.sig**2 )
 
-def gaussian( x, mu, sig ):
+    def train( self ):
+        
+        # Stochastic Gradient Descent
+        # Idea from https://stackoverflow.com/questions/57759563/minimize-multivariate-function-in-tensorflow
+        opt = tf.keras.optimizers.SGD()
 
-    return tf.math.exp( -( x - mu )**2 / sig**2 )
+        # Iterate through epochs
+        for _ in range( numEpochs ):
+
+            with tf.GradientTape() as tape:
+
+                loss = self.lossFunc( x, y )
+                print(loss)
+
+            grads = tape.gradient( loss, self.variables )
+            opt.apply_gradients( zip( grads, self.variables ) )
+
+    # First plot
+    def plotFit( self, x, y, trueX, trueY ):
+
+        # Calculate manifold from parameters
+        yPred = self.estY( trueX ).numpy()
+
+        # Plot
+        plt.figure()
+        plt.scatter( x, y, color = 'g' )                        # Noisy data
+        plt.plot( trueX, yPred, color = 'r', linestyle = '--' ) # Regression manifold
+        plt.plot( trueX, trueY, color = 'b' )                   # Noiseless sinewave
+        plt.xlabel( 'x' ) 
+        plt.ylabel( 'y', rotation = 0 ) 
+        plt.title( "Fit 1" ) 
+        plt.show()
+
+    # Second plot
+    def plotBases( self, trueX ):
+
+        plt.figure()
+        plt.rc( 'axes', prop_cycle = ( cycler ('color', ['r', 'g', 'b', 'm', 'y', 'c']) ) )
+
+        # Plot each Gaussian
+        plt.plot( trueX, self.gaussian( trueX ) )
+
+        plt.xlabel( 'x' ) 
+        plt.ylabel( 'y', rotation = 0 ) 
+        plt.title( "Bases for Fit 1" ) 
+        plt.show()
 
 
 def main():
 
-    print( "Starting MSE:", lossFunc( x, y ).numpy() )
+    model = linRegMod()
 
-    # Stochastic Gradient Descent
-    # Idea from https://stackoverflow.com/questions/57759563/minimize-multivariate-function-in-tensorflow
-    opt = tf.keras.optimizers.SGD()
-    varList = [ w, mu, sig, b ]
+    print( "Starting MSE:", model.lossFunc( x, y ).numpy() )
+    model.train()
+    print( "Final MSE:", model.lossFunc( x, y ).numpy() )
 
-    # Iterate through epochs
-    for _ in range( numEpochs ):
-
-        with tf.GradientTape() as tape:
-
-            loss = lossFunc( x, y )
-            print(loss)
-
-        grads = tape.gradient( loss, varList )
-        opt.apply_gradients( zip( grads, varList ) )
-
-    print( "Final MSE:", lossFunc( x, y ).numpy() )
-
-    # Calculate y from training data
-    yPred = tf.map_fn( estY, trueX )
-
-    # First plot
-    plt.figure()
-    plt.scatter( x, y, color = 'g' )                        # Noisy data
-    plt.plot( trueX, yPred, color = 'r', linestyle = '--' ) # Regression manifold
-    plt.plot( trueX, trueY, color = 'b' )                   # Noiseless sinewave
-    plt.xlabel( 'x' ) 
-    plt.ylabel( 'y', rotation = 0 ) 
-    plt.title( "Fit 1" ) 
-    plt.show()
-
-    # Second plot
-    plt.figure()
-    plt.rc( 'axes', prop_cycle = ( cycler ('color', ['r', 'g', 'b', 'm', 'y', 'c']) ) )
-
-    for j in range( M ):
-        plt.plot( trueX, gaussian( trueX, mu[j], sig[j] ) )
-
-    plt.xlabel( 'x' ) 
-    plt.ylabel( 'y', rotation = 0 ) 
-    plt.title( "Bases for Fit 1" ) 
-    plt.show()
-
+    model.plotFit( x, y, trueX, trueY )
+    model.plotBases( trueX )
 
 main()
